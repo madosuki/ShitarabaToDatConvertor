d51b0ee5852311ba6d3547617bca7b6d33fe9d84
madosuki <yurikasamaaikatsu@gmail.com>
2019-06-03 20:55:42 +0900

    init

---
+++ src/testShitarabaToDat/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+open FSharp.Control.Tasks.V2
+open libShitarabaToDat.shitarabaToDatTools
+
+[<EntryPoint>]
+let main argv =
+
+    let url = "https://jbbs.shitaraba.net/bbs/read_archive.cgi/otaku/14796/1504176296/"
+    let c = htmlToDatClass(url)
+    let r = c.htmlToDat.GetAwaiter().GetResult()
+    
+    (*
+    for i in r do
+        printfn "%A" i
+    *)
+
+    0 // return an integer exit code
