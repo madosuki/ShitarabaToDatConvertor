3c2b5b89cc44d30500b300db8900817a7fbe022d
madosuki <yurikasamaaikatsu@gmail.com>
2019-07-06 16:31:59 +0900

    change process

--- src/testShitarabaToDat/Program.fs
+++ src/testShitarabaToDat/Program.fs
@@ -8,20 +8,25 @@ type options = {
     [<Option("encoding", HelpText = "DAT File Encoding (e.g. sjis). Default value is utf-8. Used by System.Text.Encoding.GetEncoding()")>] encoding : string
 }
 
-let run (url: string) (encode: 'T) =
+let nullCheck x =
+    if x <> null then
+        Some(x)
+    else
+        None
+
+let run (url: string) (encode: string option) =
     let c = shitarabaToDatClass(url)
     let r = c.htmlToDat.GetAwaiter().GetResult()
 
-    if encode = null then
-        c.WriteToFile("utf-8")
-    else if encode.GetType() = typeof<string> then
-        c.WriteToFile(encode)
+    match encode with
+    | Some(encode) -> c.WriteToFile(encode)
+    | None -> c.WriteToFile("utf-8")
     
 [<EntryPoint>]
 let main argv =
     let result = CommandLine.Parser.Default.ParseArguments<options>(argv)
     match result with
-    | :? Parsed<options> as parsed -> run parsed.Value.url parsed.Value.encoding
+    | :? Parsed<options> as parsed -> run parsed.Value.url (nullCheck parsed.Value.encoding)
     | :? NotParsed<options> as notParsed -> printfn "%A" notParsed.Errors
 
     // let url = "https://jbbs.shitaraba.net/bbs/read_archive.cgi/otaku/14796/1504176296/"
