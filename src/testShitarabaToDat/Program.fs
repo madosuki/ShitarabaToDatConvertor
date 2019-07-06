58f9f2e50abf05ff5c3d3d903407512949c2ef31
madosuki <yurikasamaaikatsu@gmail.com>
2019-07-06 16:20:11 +0900

    add command line option

--- src/testShitarabaToDat/Program.fs
+++ src/testShitarabaToDat/Program.fs
@@ -1,14 +1,33 @@
 ﻿open System
 open FSharp.Control.Tasks.V2
+open CommandLine
 open libShitarabaToDat.shitarabaToDatTools
 
-[<EntryPoint>]
-let main argv =
+type options = {
+    [<Option("url", Required = true, HelpText = "Target URL")>] url : string
+    [<Option("encoding", HelpText = "DAT File Encoding (e.g. sjis). Default value is utf-8. Used by System.Text.Encoding.GetEncoding()")>] encoding : string
+}
 
-    let url = "https://jbbs.shitaraba.net/bbs/read_archive.cgi/otaku/14796/1504176296/"
+let run (url: string) (encode: 'T) =
     let c = shitarabaToDatClass(url)
     let r = c.htmlToDat.GetAwaiter().GetResult()
-    c.WriteToFile("sjis")
+
+    if encode = null then
+        c.WriteToFile("utf-8")
+    else if encode.GetType() = typeof<string> then
+        c.WriteToFile(encode)
+    
+[<EntryPoint>]
+let main argv =
+    let result = CommandLine.Parser.Default.ParseArguments<options>(argv)
+    match result with
+    | :? Parsed<options> as parsed -> run parsed.Value.url parsed.Value.encoding
+    | :? NotParsed<options> as notParsed -> printfn "%A" notParsed.Errors
+
+    // let url = "https://jbbs.shitaraba.net/bbs/read_archive.cgi/otaku/14796/1504176296/"
+    // let c = shitarabaToDatClass(url)
+    // let r = c.htmlToDat.GetAwaiter().GetResult()
+    // c.WriteToFile("sjis")
     
     (*
     for i in r do
