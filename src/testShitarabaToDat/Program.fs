9105e70b9dc89e6da2c1d40a3e58a4a81b27b079
madosuki <yurikasamaaikatsu@gmail.com>
2019-07-24 22:09:41 +0900

    Delete useless comment

--- src/testShitarabaToDat/Program.fs
+++ src/testShitarabaToDat/Program.fs
@@ -29,14 +29,4 @@ let main argv =
     | :? Parsed<options> as parsed -> run parsed.Value.url (nullCheck parsed.Value.encoding)
     | :? NotParsed<options> as notParsed -> printfn "%A" notParsed.Errors
 
-    // let url = "https://jbbs.shitaraba.net/bbs/read_archive.cgi/otaku/14796/1504176296/"
-    // let c = shitarabaToDatClass(url)
-    // let r = c.htmlToDat.GetAwaiter().GetResult()
-    // c.WriteToFile("sjis")
-    
-    (*
-    for i in r do
-        printfn "%A" i
-    *)
-
     0 // return an integer exit code
